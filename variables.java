0dfd968b14b4bd1b0902c5c29b71ad492d9b0fe6
Mely183 <melissa.villarreal183@hotmail.com>
2022-06-28 15:27:33 -0500

    Como realizar variables

---
+++ variables.java
@@ -0,0 +1,23 @@
+//Variables en java
+//es un espacio en la memoria donde se guardan datos
+
+
+public class variables {
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed;
+        //Asignamos un valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Melissa Villarreal";
+        System.out.println(employeeName);
+    }
+}
+
+//public static void main(String[] args) para poner todo eso
+//basta con solo escribir "main"
+
+// no se pueden repetir el nombre de las variables
+
