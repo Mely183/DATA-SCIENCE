09baaca173bc4bca5b9b8d7e83fcd352198323df
Mely183 <melissa.villarreal183@hotmail.com>
2022-06-28 14:57:02 -0500

    Inicios en JAVA

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+
+    }
+
+}
+/* si usamos el atajo "sout" podemos imprimir cualquier texto */
