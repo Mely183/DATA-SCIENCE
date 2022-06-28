c284fe6e7c943fb71ad07daf471b684eebd2db57
Melissa Villarreal C <87094406+Mely183@users.noreply.github.com>
2022-06-28 16:35:01 -0500

    Add files via upload

---
+++ NamingJava.java
@@ -0,0 +1,36 @@
+            //Upper Camel Case
+            //el nombre que aparece después de public class
+            //debe ser el mismo que el del archivo
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 822918372;
+        int celPhone = 238919238;
+        //Si no ponemos la variable como esta establecida
+        //marcara error
+        System.out.println(celPhone);
+        System.out.println(celphone);
+
+        String $countryName = "México";
+        String _backgroundColor = "Blue";
+
+        String currency$ = "MXN";
+        String background_color = "Purple";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+        String fullName = "Jessica Melissa Villarreal";
+        int sizeInCentimeters = 25;
+
+
+
+
+    }
+}
+
+// JAVA es sensible a las mayúsculas y minúsculas
+//todas las variables deben comenzar con una letra, $ o "_"
+//Letras posteriores pueden ser letras, números, $ y "_"
+//Las constantes se escriben en mayúscula y contienen "_"
--- UpdatingVariables.java
+++ UpdatingVariables.java
@@ -1,2 +1,29 @@
 public class UpdatingVariables {
+    public static void main(String[] args) {
+        int salary = 1000;
+
+        //bono $200
+        salary = salary + 200;
+
+        System.out.println("Tu salario es de: " + salary);
+
+        //pension: $50 descuento
+
+        salary = salary - 50;
+        System.out.println("Tu saldo final es de: " + salary);
+
+        // 2 horas extra $30 c/u
+        //Comida: $45
+
+        salary = salary + (30*2) - 45;
+        System.out.println("Tu saldo final es de: " + salary);
+
+        //Actualizando cadenas de texto
+        String employeeName = "Melissa Villarreal";
+        employeeName = employeeName + " Campos";
+        System.out.println(employeeName);
+
+    }
 }
+// Es importante reutilizar las variables para no desperdiciar
+//memoria de RAM
