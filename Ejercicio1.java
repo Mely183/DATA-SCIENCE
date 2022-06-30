ff3f78fa616184c38f7c33a60ba18ab0e0db5303
Melissa Villarreal C <87094406+Mely183@users.noreply.github.com>
2022-06-30 11:11:14 -0500

    FAMILIA
    En este ejercicio pusimos información como el nombre, la edad y la estatura de nuestros familiares.

---
+++ Ejercicio1.java
@@ -0,0 +1,22 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String mother = "Norma";
+        int ageMother = 47;
+        double heightMother = 1.60;
+
+        String father = "Oscar";
+        int ageFather = 49;
+        double heightFather = 1.65;
+
+        String brother = "Oscar";
+        int ageBrother = 28;
+        double heightBrother = 1.75;
+
+        System.out.println("Mi madre se llama " + mother + " y tiene " + ageMother + " y tiene una estatura de: " + heightMother);
+        System.out.println("Mi padre se llama " + father + " y tiene " + ageFather + " y tiene una estatura de: " + heightFather);
+        System.out.println("Mi hermano se llama " + brother + " y tiene " + ageBrother + " y tiene una estatura de: " + heightBrother);
+
+
+
+    }
+}
